5c06c23b2836a9e1e41d581101c0ad5b1d463ac2
EVNEVN <45115389+EVNEVN@users.noreply.github.com>
2019-12-28 18:28:02 +0700

    Delete bot_referral_mini.js

--- bot_referral_mini.js
+++
@@ -1,3 +0,0 @@
-$.get( "https://raw.githubusercontent.com/EVNEVN/luckygames/master/bot_referral", function( data, textStatus, jqxhr ) {
-	document.body.appendChild(document.createElement('script')).text=data;
-});
